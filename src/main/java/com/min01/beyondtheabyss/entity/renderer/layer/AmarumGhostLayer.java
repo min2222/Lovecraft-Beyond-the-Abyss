438f26aefded3cd08c61122f89fd3329db998fbc
min <goyoumin2@gmail.com>
2024-07-01 19:39:36 +0900

    fix

--- src/main/java/com/min01/beyondtheabyss/entity/renderer/layer/AmarumGhostLayer.java
+++ src/main/java/com/min01/beyondtheabyss/entity/renderer/layer/AmarumGhostLayer.java
@@ -22,7 +22,7 @@ public class AmarumGhostLayer extends GlowingLayer<EntityAmarumGhost, ModelAmaru
 	@Override
 	public void render(PoseStack p_117349_, MultiBufferSource p_117350_, int p_117351_, EntityAmarumGhost p_117352_, float p_117353_, float p_117354_, float p_117355_, float p_117356_, float p_117357_, float p_117358_)
 	{
-    	ResourceLocation texture = new ResourceLocation(String.format(BeyondtheAbyss.MODID, "textures/entity/amarum_ghost_layer.png"));
+    	ResourceLocation texture = new ResourceLocation(BeyondtheAbyss.MODID, "textures/entity/amarum_ghost_layer.png");
 		coloredGlowingModelCopyLayerRender(this.getParentModel(), this.model, texture, p_117349_, p_117350_, p_117351_, p_117352_, p_117353_, p_117354_, p_117356_, p_117357_, p_117358_, p_117355_, 1, 1, 1);
 	}
 	
