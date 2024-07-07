65d34fe859fcad4ee7de311879245e7fee2e6f32
min <goyoumin2@gmail.com>
2024-07-07 19:02:26 +0900

    fix

--- src/main/java/com/min01/beyondtheabyss/gui/overlay/HallucinationOverlay.java
+++ src/main/java/com/min01/beyondtheabyss/gui/overlay/HallucinationOverlay.java
@@ -2,6 +2,7 @@ package com.min01.beyondtheabyss.gui.overlay;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.min01.beyondtheabyss.BeyondtheAbyss;
 import com.min01.beyondtheabyss.effect.BTAEffects;
@@ -31,18 +32,23 @@ public class HallucinationOverlay
 	        RenderSystem.setShaderTexture(0, getTexture(level));
 			GuiComponent.blit(poseStack, screenWidth / 2 - 32, screenHeight / 2 - 32, 0, 0, 64, 64, 64, 64);
 			
-			if(player.tickCount % 20 == 0)
+			if(player.tickCount % 30 == 0 && !BTAClientUtil.MC.isPaused())
 			{
-				Eyes eyes = new Eyes(level.random.nextInt(screenWidth), level.random.nextInt(screenHeight), 0, level.random.nextInt(3) + 1);
+				Random random = new Random();
+				Eyes eyes = new Eyes(level.random.nextInt(screenWidth), level.random.nextInt(screenHeight), 3, level.random.nextInt(3) + 1, random.nextFloat(0.001F, 0.01F));
 				EYES.add(eyes);
 			}
 			
 			EYES.forEach(t -> 
 			{
-				t.tick();
+				if(!BTAClientUtil.MC.isPaused())
+				{
+					t.tick();
+				}
 				t.draw(poseStack, screenWidth, screenHeight);
 			});
-			EYES.removeIf(t -> t.alpha <= 0.0F);
+			
+			EYES.removeIf(t -> t.remove);
 		}
 		else if(!EYES.isEmpty())
 		{
@@ -56,27 +62,66 @@ public class HallucinationOverlay
 		public int posY;
 		public int size;
 		public int number;
-		public float alpha = 0.5F;
+		public float alpha;
+		public float amount;
+		public boolean decrease;
+		public boolean remove;
 		
-		public Eyes(int posX, int posY, int size, int number) 
+		public Eyes(int posX, int posY, int size, int number, float amount) 
 		{
 			this.posX = posX;
 			this.posY = posY;
 			this.size = size;
 			this.number = number;
+			this.amount = amount;
 		}
 		
 		public void tick()
 		{
-			this.alpha -= 0.01F;
+			if(this.alpha <= 1.0F && !this.decrease)
+			{
+				this.alpha += this.amount;
+			}
+			
+			if(this.alpha >= 1.0F)
+			{
+				this.decrease = true;
+			}
+			
+			if(this.decrease && this.alpha > 0.0F)
+			{
+				this.alpha -= this.amount;
+			}
+			
+			if(this.decrease && this.alpha <= 0.0F)
+			{
+				this.remove = true;
+			}
 		}
 		
 		public void draw(PoseStack stack, int screenWidth, int screenHeight)
 		{
-	        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-	        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
-	        RenderSystem.setShaderTexture(0, new ResourceLocation(String.format("%s:textures/effect/small_eye_%d.png", BeyondtheAbyss.MODID, this.number)));
-			GuiComponent.blit(stack, (screenWidth - this.posX) - 16, (screenHeight - this.posY) - 16, 0, 0, 32, 32, 32, 32);
+			if(this.number == 1)
+			{
+		        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
+		        RenderSystem.setShaderTexture(0, new ResourceLocation(String.format("%s:textures/effect/small_eye_%d.png", BeyondtheAbyss.MODID, 1)));
+				GuiComponent.blit(stack, (screenWidth - this.posX) - 9 * this.size / 2, (screenHeight - this.posY) - 6 * this.size / 2, 0, 0, 9 * this.size, 6 * this.size, 9 * this.size, 6 * this.size);
+			}
+			else if(this.number == 2)
+			{
+		        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
+		        RenderSystem.setShaderTexture(0, new ResourceLocation(String.format("%s:textures/effect/small_eye_%d.png", BeyondtheAbyss.MODID, 2)));
+				GuiComponent.blit(stack, (screenWidth - this.posX) - 9 * this.size / 2, (screenHeight - this.posY) - 9 * this.size / 2, 0, 0, 9 * this.size, 9 * this.size, 9 * this.size, 9 * this.size);
+			}
+			else if(this.number == 3)
+			{
+		        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
+		        RenderSystem.setShaderTexture(0, new ResourceLocation(String.format("%s:textures/effect/small_eye_%d.png", BeyondtheAbyss.MODID, 3)));
+				GuiComponent.blit(stack, (screenWidth - this.posX) - 17 * this.size / 2, (screenHeight - this.posY) - 7 * this.size / 2, 0, 0, 17 * this.size, 7 * this.size, 17 * this.size, 7 * this.size);
+			}
 		}
 	}
 	
