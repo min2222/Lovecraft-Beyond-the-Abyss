4172ae306eb0e5325a426d75e1b6dfbbd8f7e2af
min <goyoumin2@gmail.com>
2024-09-13 13:04:29 +0900

    randomized rot soil

--- src/main/java/com/min01/beyondtheabyss/block/deepabyss/RotSoilBlock.java
+++ src/main/java/com/min01/beyondtheabyss/block/deepabyss/RotSoilBlock.java
@@ -3,9 +3,6 @@ package com.min01.beyondtheabyss.block.deepabyss;
 import java.util.Arrays;
 import java.util.List;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
@@ -15,7 +12,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
-//FIXME soil type not randomized when generated via chunk
 public class RotSoilBlock extends Block
 {
 	public static final EnumProperty<SoilType> SOIL_TYPE = EnumProperty.create("soil_type", SoilType.class);
@@ -31,23 +27,9 @@ public class RotSoilBlock extends Block
 		return this.randomizeSoil(this.defaultBlockState());
 	}
 	
-	//TODO temp method
-	@SuppressWarnings("deprecation")
-	@Override
-	public void randomTick(BlockState p_222508_, ServerLevel p_222509_, BlockPos p_222510_, RandomSource p_222511_)
-	{
-		if(p_222508_.getValue(SOIL_TYPE) == SoilType.VARIANT_1) 
-		{
-			if(p_222509_.isAreaLoaded(p_222510_, 1))
-			{
-				p_222509_.setBlockAndUpdate(p_222510_, this.randomizeSoil(p_222508_));
-			}
-		}
-	}
-	
 	public BlockState randomizeSoil(BlockState state)
 	{
-		List<SoilType> list = Arrays.asList(SoilType.VARIANT_2, SoilType.VARIANT_3, SoilType.VARIANT_4);
+		List<SoilType> list = Arrays.asList(SoilType.VARIANT_1, SoilType.VARIANT_2, SoilType.VARIANT_3, SoilType.VARIANT_4);
 		int random = (int) Math.floor(Math.random() * list.size());
 		if(Math.random() <= 0.01F)
 		{
